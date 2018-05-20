a969a1a18f17bef35fa89ca8b85d387293a41357
Noah Holland <nholland2@wisc.edu>
2018-05-20 13:41:34 -0500

    Redid arts parser

--- uga/arts_parser.py
+++ uga/arts_parser.py
@@ -5,54 +5,52 @@ from bs4 import BeautifulSoup
 import csv
 import html
 
-pages=[]
-names=[]
+pages     = []
+names     = []
 positions = []
-emails = []
-name_boxes = []
-position_boxes = []
-email_boxes = []
-quote_page = input('URL: ')
-# quote_page = "http://www.agriculture.vsu.edu/faculty-and-staff/academic-faculty-staff.php"
+emails    = []
+directory_page = input('URL: ')
 
 # query the website and return the html to the variable 'page'
-page = urlopen(quote_page)
+page = urlopen(directory_page)
 
 # parse the html using beautiful soup and store in variable 'soup'
-soup = BeautifulSoup(page, 'html.parser')
+soup = BeautifulSoup(page, 'html.parser').find(class_="views-responsive-grid")
 
-dept = "Lamar Dodd School of Art"
+dept = "Art"
 
+# Get all entries in the table
+faculty = soup.find_all(class_="views-column")
 
-name_boxes.extend(soup.find_all(class_="views-field-field-last-name"))
-try:
-     position_boxes.extend(soup.find_all(class_= re.compile("views-field-field-job-title")))
-except Exception:
-     print("Manually check positions, there's a mismatch")
-     positions.append("")
+i = 1;
 
-email_boxes.extend(soup.find_all(class_="views-field-field-email"))
-#email_boxes.extend(soup.find_all(href=re.compile("mailto"))) # look for attributes with href = "mailto..."
-#sections.append(sections[x].find("a").attrs['name'])
-
-for x in range(0,len(name_boxes)):
-    names.append(name_boxes[x].text.strip())
+for person in faculty:
+    name = person.find(class_="views-field-field-last-name").text
+    name = re.sub(" +"," ",name).strip()
+    names.append(name)
     try:
-     positions.append(position_boxes[x].findChild().string)
-    except Exception:
-     print("Manually check positions, there's a mismatch")
-     positions.append("")
-
+        emails.append(html.unescape(re.search(">(.+)</a>",person.find(class_="views-field-field-email").text).group(1)))
+    except:
+        print("\rEmail not found for {}".format(names[-1]))
+        emails.append("")
     try:
-     emails.append(html.unescape(re.search(">(.+)</a>",email_boxes[x].findChild().text).group(1)))
-    except Exception:
-     print("Manually check emails, there's a mismatch")
-     emails.append("")
+        position = person.find(class_="views-field-field-job-title").text.strip()
+        if not position:
+            position = person.find(class_="views-field-field-job-title-1").text.strip()
+        positions.append(position)
+    except:
+        print("\rPosition not found for {}".format(names[-1]))
+        positions.append("")
 
-    #semails.append(email_boxes[x].string)
-    # emails.append(email_boxes[x].attrs["href"][7:]) # [7:] cuts off first 7 chacters "mailto:"
+    print("\r{}/{}".format(i,len(faculty)), end = "", flush=True)
+    i += 1
+
+print()
 
 with open('arts_output.csv', 'a+', newline='') as csvfile:
     writer = csv.writer(csvfile)
     for x in range(0,len(names)):
-        writer.writerow([names[x],emails[x],positions[x],dept])
+        try:
+            writer.writerow([names[x],emails[x],positions[x],dept])
+        except:
+            print("There was an issue writing data for {}".format(names[x]))
