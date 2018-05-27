2e45364f163257b9fddfa433cbae57b75fd97aa1
noah <moah909@gmail.com>
2018-05-27 14:29:41 -0500

    Adding misc tools

---
+++ misc/standardpositions.py
@@ -0,0 +1,32 @@
+import csv
+import sys
+
+mapping = {}
+
+if (len(sys.argv) != 4) or ("-h" in sys.argv) or ("--help" in sys.argv):
+    print("Usage: {} <input file> <position mappings> <output file>\nReplaces positions in <input file> with mappings from <positions mappings> and outputs them in <output file>".format(sys.argv[0]))
+    exit()
+
+positions = set()
+
+with open(sys.argv[1]) as datafile, open(sys.argv[2]) as mappingfile, open(sys.argv[3], 'w')as outputfile:
+    datareader = csv.reader(datafile)
+    writer = csv.writer(outputfile)
+    posmap = csv.reader(mappingfile)
+    # posmapw = csv.writer(mappingfile)
+    mapping = {rows[0]:rows[1] for rows in posmap}
+
+    for row in datareader:
+        try:
+            if mapping[row[2]] == "--":
+                continue
+        except KeyError:
+            print("Missing mapping for: {}".format(repr(row[2])))
+            positions.add(row[2])
+            continue
+
+        row[2] = mapping[row[2]]
+        writer.writerow(row)
+
+    # for x in positions:
+    #     posmapw.writerow([x,""])
---
+++ misc/uniquepositions.py
@@ -0,0 +1,18 @@
+import csv
+import sys
+
+if (len(sys.argv) != 3) or ("-h" in sys.argv) or ("--help" in sys.argv):
+    print("Usage: {} <input file> <output file>\nGenerates a list of unique positions in <input file> and outputs them in <output file>".format(sys.argv[0]))
+    exit()
+
+positions = set()
+
+with open(sys.argv[1]) as csvfile:
+    filereader = csv.reader(csvfile)
+    for row in filereader:
+        positions.add(row[2])
+
+with open(sys.argv[2], 'a+', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    for x in positions:
+        writer.writerow([x,""])
