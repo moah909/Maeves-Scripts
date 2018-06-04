d696481b1184b228e0f6e1917a666a781ec77ce2
noah <moah909@gmail.com>
2018-06-04 02:07:15 -0500

    pushing latest sequential one

--- rut/rut_parser.py
+++ rut/rut_parser.py
@@ -5,6 +5,8 @@ import sys
 from multiprocessing import Pool, Queue
 sys.path.insert(0,'../')
 from common.common import *
+from urllib.request import urlopen, Request
+from random import randrange
 import requests
 from bs4 import BeautifulSoup
 import csv
@@ -12,9 +14,11 @@ import itertools
 
 def getInfoFromSubpage(link):
     for attempt in range(0,5):
-        time.sleep(5)
         try:
-            soup     = BeautifulSoup(requests.get(link).text,"html.parser")
+            soup = BeautifulSoup(requests.get(link, proxies = {"http":proxies[randrange(100)]}, timeout=30).text,"html.parser")
+            email    = soup.find(href=re.compile("mailto")).text.strip()
+            position = soup.find("dd").text.strip()
+            return (email,position)
         except:
             print("{} failed to open {}/5, retrying...".format(query,attempt,))
             time.sleep(attempt)
@@ -23,11 +27,8 @@ def getInfoFromSubpage(link):
             break
     else:
         raise Exception("Failed 5 times")
-    email    = soup.find(href=re.compile("mailto")).text.strip()
-    position = soup.find("dd").text.strip()
-    return (email,position)
-
 def readPage(query):
+    print("Query: {}".format(query))
     current_page = 0
     last_page = -1
     retval = []
@@ -36,7 +37,6 @@ def readPage(query):
              "p_name_last":query }
 
     for attempt in range(0,5):
-        time.sleep(5)
         try:
             response = requests.post(directory_page,data=data)
             response.raise_for_status()
@@ -77,8 +77,25 @@ def readPage(query):
             dept = ""
 
         print("\r{}/{}".format(idx,len(faculty)), flush=True, end="")
-
+        past_queries.append(query)
         retval.append([name,email,position,dept])
+
+
+    if soup.find(text=re.compile("50 matches")) != None:
+        print()
+        last_name = faculty[-1].td.text.strip().split(" ")[-1]
+        if len(query) + 1 < len(last_name):
+            new_query = last_name[0:len(query)+1]
+        else:
+            new_query = last_name
+        while True:
+
+            if new_query not in past_queries:
+                readPage(new_query)
+
+            if new_query[-1] == "z":
+                break
+            new_query = new_query[:-1] + ALPHABET[ord(new_query[-1])-ord("a")+1]
 
 
     print("\r{}/{} {}".format(len(faculty),len(faculty),query), flush=True)
@@ -110,7 +127,7 @@ position_attrs  = { "text" : "Title:"}
 dept_tag        = None # Can be None to not sort by tag
 dept_attrs      = { "text" : "Dept:"}
 
-output_file = "umn.csv"
+output_file = "rut.csv"
 
 ALPHABET = "abcdefghijklmnopqrstuvwxyz"
 
@@ -118,19 +135,38 @@ CHUNK_SIZE  = 26
 START_INDEX = 0
 
 queries = [ "".join(tup) for tup in itertools.product(ALPHABET,repeat=2) ]
+proxies = []
+
+past_queries = []
+
+req = Request( "https://sslproxies.org/",
+   data=None,
+    headers={
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
+    }
+)
+
+page = urlopen(req)
+
+proxysoup = BeautifulSoup(page,"html.parser")
+proxies_table = proxysoup.find(id='proxylisttable')
+
+for row in proxies_table.tbody.find_all('tr'):
+    proxies.append( "http://" + row.find_all('td')[0].string + ":" +
+      row.find_all('td')[1].string)
 
 for i in range(0,26):
-    try:
-        result = []
-        # with Pool() as pool:
-        for j in range(0,26):
-            result.append(readPage(queries[i*26+j]))
+    # try:
+    result = []
+    # with Pool() as pool:
+    for j in range(0,26):
+        result.append(readPage(queries[i*26+j]))
 
-        with open(ALPHABET[i] + "." + output_file, 'a+', newline='') as csvfile:
-            writer = csv.writer(csvfile)
-            for process in result:
-                for row in process:
-                    writer.writerow(row)
-    except Exception as e:
-        print(e)
-        print("Error was caught on iteration {}/{}".format(i,25))
+    with open(ALPHABET[i] + "." + output_file, 'a+', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        for process in result:
+            for row in process:
+                writer.writerow(row)
+    # except Exception as e:
+    #     print(e)
+    #     print("Error was caught on iteration {}/{}".format(i,25))
